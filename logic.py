a5da59a27f0196da35f013cf220de2abb80177bc
MiriSel <miris6474@gmail.com>
2026-05-03 04:54:55 +0000

    same prob, same fix

--- logic.py
+++ logic.py
@@ -82,7 +82,7 @@ def query_pipeline(current_education, goal_salary, skill_one, skill_two, skill_t
 
 
 def query_average_skill_with_education(education):
-    conn = sqlite3.connect("/workspaces/Career-Ideas/job_list.db")
+    conn = sqlite3.connect("job_list.db")
     cur = conn.cursor()
     with conn:
         cur.execute("""
